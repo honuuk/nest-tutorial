7819958a1049d5e77df2528e97645ca8b4cef904
jewook <jewook3617@gmail.com>
2021-09-10 16:57:36 +0900

    add: env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
+
 import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [PostsModule],
+  imports: [
+    PostsModule,
+    ConfigModule.forRoot({
+      validationSchema: {
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USER: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+        PORT: Joi.number(),
+      },
+    }),
+  ],
   controllers: [],
   providers: [],
 })
