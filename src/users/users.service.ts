9bdb6aa6040195bc7a399339c9670b67e59755e6
jewook <jewook3617@gmail.com>
2021-09-14 09:57:10 +0900

    add: UsersService implementation

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import User from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(@InjectRepository(User) private usersRepository: Repository<User>) {}
+
+  async getByEmail(email: string) {
+    const user = await this.usersRepository.findOne({ email });
+    if (!user) throw new HttpException('User with this email does not exist', HttpStatus.NOT_FOUND);
+    return user;
   }
 
-  findAll() {
-    return `This action returns all users`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async create(userData: CreateUserDto) {
+    const newUser = await this.usersRepository.create(userData);
+    await this.usersRepository.save(newUser);
+    return newUser;
   }
 }
